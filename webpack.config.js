f453007241b3954423a7c948b66de5257d633271
dfloresdev <hello@dflores.dev>
2020-02-22 08:59:19 -0600

    show data about API rick and morty

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -24,6 +25,12 @@ module.exports = {
       inject: true,
       template: './public/index.html',
       filename: './index.html'
-    })
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: './src/styles/styles.css',
+        to: ''
+      }
+    ])
   ]
 };
